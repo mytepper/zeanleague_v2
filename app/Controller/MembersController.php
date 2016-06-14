2fbf77130880011d61c1004383e73e2aa3006d16
mytepper <mytepper@gmail.com>
2016-06-14 17:54:25 +0700

    add member profile

--- app/Controller/MembersController.php
+++ app/Controller/MembersController.php
@@ -5,7 +5,16 @@ App::uses('AppController', 'Controller');
  */
 class MembersController extends AppController {
 
-public $components = array('RequestHandler', 'Flash', 'Paginator');
+	public $components = array('RequestHandler', 'Paginator');
+
+/**
+ * [beforeFilter]
+ *
+ * @return void
+ */
+	public function beforeFilter() {
+		return parent::beforeFilter();
+	}
 
 /**
  * [register public page]
@@ -13,7 +22,18 @@ public $components = array('RequestHandler', 'Flash', 'Paginator');
  * @method register
  * @return void
  */
-	public function register() {
-		
+	public function profile() {
+		$user = $this->Auth->user();
+		if ($this->request->is('put')) {
+			$saved = $this->Member->save($this->request->data);
+			if ($saved) {
+				$this->Flash->success('Update profile successfully.', array('key' => 'members'));
+				$this->redirect('/members/profile');
+			} else {
+				$this->Flash->error('Please check all input of required.', array('key' => 'members'));
+			}
+		} else {
+			$this->request->data = $user;
+		}
 	}
 }
